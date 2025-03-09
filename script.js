1138d9ebd8c72961d9c8d1f3fa41bf96283c07b8
Abenezer Daniel <aabenezzer@gmail.com>
2025-03-09 12:49:14 +0300

    bulid form functionality to add a new book

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const myLibrary = [];
 
 // create a book constructor
-function Book(name, author, publishedDate, price) {
+function Book(title, author, publishedDate, price) {
   if (!new.target)
     throw Error("You have use new keyword to call Book constructor!");
   this.id = crypto.randomUUID();
-  this.name = name;
+  this.title = title;
   this.author = author;
   this.publishedDate = publishedDate;
+  this.status = "unread";
   this.price = price;
 }
 
@@ -15,17 +16,55 @@ function Book(name, author, publishedDate, price) {
 function addBookToLibrary(name, author, publishedDate, price) {
   const book = new Book(name, author, publishedDate, price);
   myLibrary.push(book);
+  displayBooks(myLibrary);
 }
 
 // display all the books to the user
-function displayBooks() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+function displayBooks(books) {
+  const tbody = document.querySelector("tbody");
+  for (let i = 0; i < books.length; i++) {
+    const tr = document.createElement("tr");
+    const tds = `
+      <td>${books[i].title}</td>
+      <td>${books[i].author}</td>
+      <td>${books[i].publishedDate}</td>
+      <td>${books[i].price}</td>
+      <td>${books[i].status}</td>
+      <td class="action-td">
+      <button data-id=${books[i].id} class="action-read">mark As Read</button>
+      <button data-id=${books[i].id} class="action-delete">Delete</button>
+       </td>
+    `;
+    tr.innerHTML = tds;
+    tbody.appendChild(tr);
+    console.log(tr);
   }
 }
 
-// Add new book modal form
+// add new book
+const form = document.querySelector("#book-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const publishedDate = formData.get("publishedDate");
+  const price = formData.get("price");
 
+  addBookToLibrary(title, author, publishedDate, price);
+
+  bgOverlay.style.display = "none";
+  modal.style.display = "none";
+});
+
+// mark books as read 
+
+function markAsRead(id) {
+  const book = myLibrary.filter(book => book.id === id);
+  console.log(book);
+}
+
+// Add new book modal form
 const addBookbtn = document.querySelector("#add-book");
 const bgOverlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
@@ -36,8 +75,8 @@ addBookbtn.addEventListener("click", (e) => {
 });
 
 // remove the modal when a user click outside the modal
-
 bgOverlay.addEventListener("click", (e) => {
   bgOverlay.style.display = "none";
   modal.style.display = "none";
 });
+
