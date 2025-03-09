0c34d557528490fd5e82a604fc5998bc79086a66
Abenezer Daniel <aabenezzer@gmail.com>
2025-03-09 11:01:28 +0300

    structure and style the site navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const myLibrary = [];
 
 // create a book constructor
-
 function Book(name, author, publishedDate, price) {
   if (!new.target)
     throw Error("You have use new keyword to call Book constructor!");
@@ -13,11 +12,14 @@ function Book(name, author, publishedDate, price) {
 }
 
 // add book to the library array
-
 function addBookToLibrary(name, author, publishedDate, price) {
   const book = new Book(name, author, publishedDate, price);
   myLibrary.push(book);
 }
 
-
-
+// display all the books to the user
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+}
