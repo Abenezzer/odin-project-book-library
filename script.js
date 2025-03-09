4e253c98943143ad8a565c898bb9dcff07df1365
Abenezer Daniel <aabenezzer@gmail.com>
2025-03-09 10:42:48 +0300

    Create addBookToLibrary function to add new book to the library

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const myLibrary = [];
 function Book(name, author, publishedDate, price) {
   if (!new.target)
     throw Error("You have use new keyword to call Book constructor!");
+  this.id = crypto.randomUUID();
   this.name = name;
   this.author = author;
   this.publishedDate = publishedDate;
   this.price = price;
 }
+
+// add book to the library array
+
+function addBookToLibrary(name, author, publishedDate, price) {
+  const book = new Book(name, author, publishedDate, price);
+  myLibrary.push(book);
+}
+
+
 
